24a3c094b35d29e57d08e1819c6adedc01e91ab0
Ajit-tiwari <tajit40.at@gmail.com>
2020-07-02 19:35:47 +0530

    Updates

---
+++ precache-manifest.56d51942886eab37c7bd1d231c729635.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a691c23b09a7a1ab3ecaa2831a8b121",
+    "url": "/Note-Taking-App/index.html"
+  },
+  {
+    "revision": "700049e5cfcf11f71b9d",
+    "url": "/Note-Taking-App/static/css/main.ae986698.chunk.css"
+  },
+  {
+    "revision": "3e8b3f13d23d72fbef03",
+    "url": "/Note-Taking-App/static/js/2.aacc1608.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/Note-Taking-App/static/js/2.aacc1608.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "700049e5cfcf11f71b9d",
+    "url": "/Note-Taking-App/static/js/main.c8f49323.chunk.js"
+  },
+  {
+    "revision": "f914d467c8c588a2e074",
+    "url": "/Note-Taking-App/static/js/runtime-main.e17e71a7.js"
+  },
+  {
+    "revision": "ce3b4d48f2e345a92647d19092fd9f9c",
+    "url": "/Note-Taking-App/static/media/logo.ce3b4d48.webp"
+  }
+]);
